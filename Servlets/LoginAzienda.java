cedf2e31c9597456870e41929c5a6df56e159a82
AlessandroDominoni <96467844+AlessandroDominoni@users.noreply.github.com>
2022-01-16 14:57:11 +0100

    Update LoginAzienda.java

--- Servlets/LoginAzienda.java
+++ Servlets/LoginAzienda.java
@@ -1,25 +1,55 @@
 import java.io.*;
 import java.util.*;
-
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.http.*;
+import java.sql.*;
 
-public class LoginAzienda extends HttpServlet {
 
-    private static final long serialVersionUID = 1L;
-
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
-        response.setContentType("text/html");
-        response.setCharacterEncoding("UTF-8");
-        PrintWriter out = response.getWriter();
-
-        out.println("<!DOCTYPE html><html>");
-        out.println("<head>");
-        out.println("<meta charset=\"UTF-8\" />");
-        out.println("<title> Login Azienda </title>");
-        out.println("</head>");
-        out.println("<body>Hai fatto l'accesso con successo");
-        out.println("</body>");
-        out.println("</html>");
+public class LoginAzienda extends HttpServlet{
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+        Connection connection=null;
+        HttpSession session = req.getSession(true);
+        PrintWriter printwriter = res.getWriter();
+        RequestDispatcher rd = req.getRequestDispatcher("scegliLinea");
+        res.setContentType("text/html");
+       
+        try {
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Errore: Impossibile caricare il Driver");
+        }
+        try {
+            connection = DriverManager.getConnection("jdbc:ucanaccess://" + req.getServletContext().getRealPath("/") + "Database1.accdb");
+           
+            String username=req.getParameter("username");
+            
+            String password=req.getParameter("password");
+            
+            String query = "SELECT username FROM Azienda WHERE username= '"+ username +"' AND password= '"+ password +"'";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            String risultato=null;
+            while(resultSet.next()){
+                risultato=resultSet.getString(1);
+            }
+            if(risultato==null){
+                printwriter.println("<p align='center'>Errore di login, credenziali errate</p> <br> <p>Torna alla schermata di <a href='index.html'>login</a></p>");
+            }
+            else{
+                res.sendRedirect(req.getContextPath()+"/home.html");
+            }
+        } catch (Exception e) {
+            System.out.println("Errore: Impossibile Connettersi al Database");
+        }
+        finally{
+            if(connection!=null){
+                try{
+                    connection.close(); 
+                }
+                catch (Exception e){
+                    System.out.println("Errore nella chiusura della connessione");
+                }
+            }
+        }
     }
 }
